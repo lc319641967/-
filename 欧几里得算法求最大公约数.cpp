1461c95d22bfae31a4bdfa84714ab8188c6c3789
lc319641967 <67952144+lc319641967@users.noreply.github.com>
2020-08-03 12:25:42 +0800

    Update 欧几里得算法求最大公约数.cpp

--- 欧几里得算法求最大公约数.cpp
+++ 欧几里得算法求最大公约数.cpp
@@ -4,13 +4,13 @@ int main()
     int a,b;
     int c=0,d=0;
     scanf("%d%d",&a,&b);（输入两个数）
-    if(a<b)
+    if(a<b)（比较大小，交换位置）
     {
     	d=a;
         a=b;
         b=d;
     }
-    while(a%b)
+    while(a%b)（辗转相除法）
     {
     	c=a%b;
     	a=b;
